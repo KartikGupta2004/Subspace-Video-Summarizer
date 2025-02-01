b138747d47c72ac2032dfe2f0f3341a28ae743b2
KartikGupta2004 <kartikgupta9867@gmail.com>
2025-02-02 01:01:46 +0530

    Added thumbnails in res

--- functions/generateSummary.js
+++ functions/generateSummary.js
@@ -16,8 +16,9 @@ export default async (req,res) => {
     const data = await response.json();
 
     return res.json({
-      summary: data.summary,
-      title: data.title,
+        summary: data.summary,
+        title: data.title,
+        thumbnails: data.thumbnails,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
